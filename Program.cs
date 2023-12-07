c722375f5e1e21be79cedc5893675d8993f88384
PoliYag <150130450+PoliYag@users.noreply.github.com>
2023-12-07 18:02:33 +0300

    task_5_3

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int minSum = int.MaxValue;
+        int minSumRow = -1;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                sum += matrix[i, j];
+            }
+
+            if (sum < minSum)
+            {
+                minSum = sum;
+                minSumRow = i;
+            }
+        }
+
+        Console.WriteLine("Строка с наименьшей суммой элементов: " + minSumRow);
+        Console.WriteLine("Сумма элементов строки: " + minSum);
+    }
+}
